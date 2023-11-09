be970de7f421874c71df7cd8e8abf3bd9d0ca41c
Charline Mons <charline.mons@gmail.com>
2023-11-09 16:51:37 -0500

    Debbug Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,50 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
-import axios from "axios";
-import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
-
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
-
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
-
-  function load() {
-    let apiKey = "1fabbbt6e694149ea2da3obbe200ebf2";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-  }
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 7) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
+export default function WeatherForecast() {
+  return (
+    <div className="next-days">
+      <h1>Next 7 days</h1>
+      <div id="forecast" className="weather-forecast">
+        <div class="col-2 forecast-days col-holder"></div>
+        <div className="weather-forecast-date">today</div>
+        <WeatherIcon code="rain-night" size={36} />
+        <div className="weather-forecast-temperatures">
+          <span className="weather-forecast-temp-max">19°</span>
+          <span className="weather-forecast-temp-min">10°</span>
         </div>
       </div>
-    );
-  } else {
-    load();
-
-    return null;
-  }
+    </div>
+  );
 }
