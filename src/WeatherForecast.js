7a57a16ef0db870242c0da6dc0ae66f882bef1b7
Charline Mons <charline.mons@gmail.com>
2023-11-09 16:41:36 -0500

    Added Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,23 +1,50 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function Weather() {
-  return (
-    <div className="next-days">
-      <h1>Next 7 days</h1>
-      <div id="forecast" className="weather-forecast">
-        <div class="col-2 forecast-days col-holder">
-          <div className="weather-forecast-date">Thu</div>
-          <span className="icon-forecast">
-            <WeatherIcon code="clear-sky-day" size={36} />
-          </span>
-          <div className="weather-forecast-temperatures">
-            <span className="weather-forecast-temp-max">12°</span>
-            <span className="weather-forecast-temp-min">15°</span>
-          </div>
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "1fabbbt6e694149ea2da3obbe200ebf2";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 7) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    load();
+
+    return null;
+  }
 }
